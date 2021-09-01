80c37743380653d1cb9d28c94c8cfe38a724b2bd
DLipson <59182090+DLipson@users.noreply.github.com>
2021-09-01 21:56:53 +0300

    initial parser-parser project commit

---
+++ Parser Parser/Parser Parser/ParseParserMethods.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser_Parser
+{
+    static public class ParseParserMethods
+    {
+        static public void Parse()
+        {
+            string origParserPath = "H:\\Documents\\Parser_orig.txt";
+            string newParserPath = "H:\\Documents\\Parser_new.txt";
+            RecordMaker.MakeRecord(origParserPath, newParserPath);
+        }
+
+        static public string GetSubString(string item, string charsBefore, string charsAfter)
+        {
+            int startIndex = item.IndexOf(charsBefore);
+            int endIndex = item.IndexOf(charsAfter);
+            int subStringStart = startIndex + 1;
+            int subStringLength = endIndex - startIndex;
+            return item.Substring(subStringStart, subStringLength);
+        }
+
+        static public void AppendToFile(string path, string textToAppend)
+        {
+
+            using StreamWriter file = new(path, append: true);
+            file.WriteLineAsync(textToAppend);
+
+        }
+    }
+
+    static public class RecordMaker
+    {
+        public static void MakeRecord(string origParserPath, string newParserPath)
+        {
+
+            string csvFieldName;
+            string recordProperty;
+            bool previousLineWasCsvField = false;
+            StreamReader reader = File.OpenText(origParserPath);
+
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Contains("CsvField"))
+                {
+                    csvFieldName = ParseCsvFieldName(line);
+                   ParseParserMethods.AppendToFile(newParserPath, $"[Name = {csvFieldName}]");
+                    previousLineWasCsvField = true;
+                }
+                else if (previousLineWasCsvField == true)
+                {
+                    recordProperty = line;
+                    ParseParserMethods.AppendToFile(newParserPath, recordProperty);
+                    previousLineWasCsvField = false;
+                }
+
+            }
+        }
+        static private string ParseCsvFieldName(string item)
+        {
+            string charsBefore = "= ";
+            string charsAfter = ")]";
+
+            return ParseParserMethods.GetSubString(item, charsBefore, charsAfter);
+        }
+
+    }
+}
---
+++ Parser Parser/Parser Parser/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Parser_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParseParserMethods.Parse();
+        }
+
+
+
+
+
+        //public void RegexParse()
+        //{
+
+        //    Regex parts = new Regex(@"^\d+\t(\d+)\t.+?\t(item\\[^\t]+\.ddj)");
+
+        //    StreamReader reader = FileInfo.OpenText("filename.txt");
+        //    string line;
+        //    while ((line = reader.ReadLine()) != null)
+        //    {
+        //        Match match = parts.Match(line);
+        //        if (match.Success)
+        //        {
+        //            int number = int.Parse(match.Group(1).Value);
+        //            string path = match.Group(2).Value;
+
+        //            // At this point, `number` and `path` contain the values we want
+        //            // for the current line. We can then store those values or print them,
+        //            // or anything else we like.
+        //        }
+        //    }
+        //}
+    }
+}
